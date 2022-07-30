9e847104ae0a2c765b02052116c39bd96e64b01d
Haramb3 <Haramb3@users.noreply.github.com>
2022-07-30 10:34:15 +0200

    Update AverageGrowth.js

--- AverageGrowth.js
+++ AverageGrowth.js
@@ -1,7 +1,7 @@
 var node = document.createElement("DIV");
-var dailyGrowth = document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[0].innerText;
-var weeklyGrowth = document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[1].innerText;
-var monthlyGrowth = document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[2].innerText;
+var dailyGrowth = document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[0].innerText;
+var weeklyGrowth = document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[1].innerText;
+var monthlyGrowth = document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[2].innerText;
 dailyGrowth = dailyGrowth.replaceAll("One day","");
 dailyGrowth = dailyGrowth.replaceAll(/(\r\n|\n|\r)/gm, "");
 dailyGrowth = dailyGrowth.replaceAll(" ", "");
@@ -20,13 +20,13 @@ monthlyGrowth = monthlyGrowth.replaceAll(" ", "");
 monthlyGrowth = monthlyGrowth.replaceAll(",",'');
 monthlyGrowth = monthlyGrowth.replaceAll("↑",'');
 monthlyGrowth = monthlyGrowth.replaceAll("↓",'');
-if(document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[0].children[1].classList.value == "bottom red") {
+if(document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[0].children[1].classList.value == "bottom red") {
 	dailyGrowth = -dailyGrowth;
 }
-if(document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[1].children[1].classList.value == "bottom red") {
+if(document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[1].children[1].classList.value == "bottom red") {
 	weeklyGrowth = -weeklyGrowth;
 }
-if(document.getElementsByClassName("box box-records")[1].children[0].getElementsByClassName("left")[2].children[1].classList.value == "bottom red") {
+if(document.getElementsByClassName("boxes-2")[0].children[0].getElementsByClassName("left")[2].children[1].classList.value == "bottom red") {
 	monthlyGrowth = -monthlyGrowth;
 }
 var averageGrowth = Math.round((+dailyGrowth + +weeklyGrowth/7 + +monthlyGrowth/30)/3);
